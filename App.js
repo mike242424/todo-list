15ce693c55fbf1bbbdc506974cd49c2beb848353
mike242424 <mikebyers24@gmail.com>
2024-03-24 14:57:26 -0400

    spacing structure

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { style } from './App.style';
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>App</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={style.app}>
+          <View style={style.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={style.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={style.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 };
 
--- App.style.js
+++ App.style.js
@@ -1,3 +1,11 @@
 import { StyleSheet } from 'react-native';
 
-export const style = StyleSheet.create({});
+export const style = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: '#F9F9F9',
+  },
+  header: { flex: 1, backgroundColor: 'blue' },
+  body: { flex: 5, backgroundColor: 'red' },
+  footer: { height: 70, backgroundColor: 'green' },
+});
